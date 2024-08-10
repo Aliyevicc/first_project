3b10e460aa9a00288ec1d02c70d643bb741dfd29
muhsin <ashurovmuhsinn@gmail.com>
2024-08-09 19:43:57 -0700

    my first commit

--- app.dart
+++ app.dart
@@ -1,1 +1,16 @@
-void main (){}
+void main (){
+  class G extends StatefulWidget {
+    const G({super.key});
+
+    @override
+    State<G> createState() => _GState();
+  }
+
+  class _GState extends State<G> {
+    @override
+    Widget build(BuildContext context) {
+      return const Placeholder();
+    }
+  }
+
+}
